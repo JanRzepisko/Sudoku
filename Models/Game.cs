16a4ed3718a29d4f7b1aac02712fe5907233d910
Jan Rzepisko <60238932+JanRzepisko@users.noreply.github.com>
2023-05-14 22:46:25 +0200

    importers

--- Models/Game.cs
+++ Models/Game.cs
@@ -6,15 +6,14 @@ namespace Sudoku.Models;
 
 public class Game : IGame
 {
-    [MaxLength(9)] private IEnumerable<Row>  Rows { get; set; }
-    [MaxLength(9)] private IEnumerable<Row>  Columns { get; set; }
-    [MaxLength(9)] private IEnumerable<Row>  Squares { get; set; }
-    [MaxLength(81)] private IEnumerable<Field> Fields { get; set; }
+    public IEnumerable<Row>  Rows { get; set; }
+    public IEnumerable<Row>  Columns { get; set; }
+    public IEnumerable<Row>  Squares { get; set; }
+    public  IEnumerable<Field> Fields { get; set; }
 
     public IReadOnlyList<IReadOnlyList<int>> Solve(IReadOnlyList<IReadOnlyList<int>> input)
     {
-        ImportData(input);
-
+        this.ImportData(input);    
         while (Fields.Any(c => c.Value == 0))
         {
             var changes = 0;
@@ -40,16 +39,18 @@ public class Game : IGame
                     SetNumber(field.FieldId, possibleValues[0]);
                     changes++;
                 }
+                else if (possibleValues.Count == 2)
+                {
+                    
+                    
+                    
+                }
             }
-
             if (changes == 0)
             {
                 break;
             }
         }
-
-
-
         return Rows.Select(c => c.Fields.Select(c => c.Value).ToList()).ToList();
     }
     public void SetNumber(int fieldId, int value)
@@ -83,132 +84,7 @@ public class Game : IGame
             }
         }
     }
-    private void ImportData(IReadOnlyList<IReadOnlyList<int>> input)
-    {
-        ImportRows(input);
-        ImportColumns(input);
-        ImportSquare();
-        ImportFields(input);
-    }
-
-    private void ImportRows(IReadOnlyList<IReadOnlyList<int>> input)
-    {
-        var rows = new List<Row>();
-        for (var i = 0; i < 9; i++)
-        {
-            var row = new Row();
-            var fields = new List<Field>();
-            for (var j = 0; j < 9; j++)
-            {
-                var field = new Field()
-                {
-                    Value = input[i][j],
-                    isOrginalValue = input[i][j] != 0,
-                    FieldId = (((i + 1) * 9) + j) - 9
-                };
-                fields.Add(field);
-            }
-
-            row.Fields = fields;
-            rows.Add(row);
-        }
-
-        
-        Rows = rows;
-    }
-
-    private void ImportColumns(IReadOnlyList<IReadOnlyList<int>> input)
-    {
-        var columns = new List<Row>();
-        for (var i = 0; i < 9; i++)
-        {
-            var column = new Row();
-            var fields = new List<Field>();
-            for (var j = 0; j < 9; j++)
-            {
-                var field = new Field()
-                {
-                    Value = input[j][i],
-                    isOrginalValue = input[j][i] != 0,
-                    FieldId = (((j + 1) * 9) + i) - 9
-                };
-                fields.Add(field);
-            }
-
-            column.Fields = fields;
-            columns.Add(column);
-        }
-
-        Columns = columns;
-    }
-
-    private void ImportSquare()
-    {
-        var squares = new List<List<Field>>();
-        var bigSquares = new List<List<Field>>();
-        var finalSquares = new List<List<Field>>();
-
-        for (int i = 0; i < 3; i++)
-        {
-            bigSquares.Add(new List<Field>());
-        }
-
-        for (var i = 0; i < 9; i++)
-        {
-            for (var j = 0; j < 3; j++)
-            {
-                squares.Add(Columns.ToList()[i].Fields.Skip(j * 3).Take(3).ToList());
-            }
-        }
-
-        for (var i = 0; i < 27; i++)
-        {
-            bigSquares[i % 3].AddRange(squares[i]);
-        }
-
-        for (var i = 0; i < 3; i++)
-        {
-            for (var j = 0; j < 9; j++)
-            {
-                finalSquares.Add(bigSquares[i].Skip(j * 3).Take(3).ToList());
-            }
-        }
-
-        var result = new List<Row>();
-
-        for (var i = 0; i < 9; i++)
-        {
-            var fields = new List<Field>();
-            for (var j = 0; j < 3; j++)
-            {
-                fields.AddRange(finalSquares[i * 3 + j]);
-            }
-
-            result.Add(new Row() { Fields = fields });
-        }
-
-        Squares = result.Select(x => new Row() { Fields = x.Fields.OrderBy(c => c.FieldId).ToList() });
-
-    }
-
-    private void ImportFields(IReadOnlyList<IReadOnlyList<int>> input)
-    {
-        var fields = new List<Field>();
-        for (var i = 0; i < 9; i++)
-        {
-            for (var j = 0; j < 9; j++)
-            {
-                fields.Add(new Field()
-                {
-                    Value = input[j][i],
-                    isOrginalValue = input[j][i] != 0,
-                    FieldId = (((j + 1) * 9) + i) - 9
-                });
-            }
-        }
-
-        Fields = fields;
-    }
+    
 }
 /*
 [
---
+++ Models/GameImporter.cs
@@ -0,0 +1,131 @@
+﻿namespace Sudoku.Models;
+
+public static class GameImporter
+{
+    public static void ImportData(this Game game, IReadOnlyList<IReadOnlyList<int>> input)
+    {
+        ImportRows(game, input);
+        ImportColumns(game, input);
+        ImportSquare(game);
+        ImportFields(game, input);
+    }
+
+    private static void ImportRows(this Game game, IReadOnlyList<IReadOnlyList<int>> input)
+    {
+        var rows = new List<Row>();
+        for (var i = 0; i < 9; i++)
+        {
+            var row = new Row();
+            var fields = new List<Field>();
+            for (var j = 0; j < 9; j++)
+            {
+                var field = new Field()
+                {
+                    Value = input[i][j],
+                    isOrginalValue = input[i][j] != 0,
+                    FieldId = (((i + 1) * 9) + j) - 9
+                };
+                fields.Add(field);
+            }
+
+            row.Fields = fields;
+            rows.Add(row);
+        }
+
+        
+        game.Rows = rows;
+    }
+
+    private static void ImportColumns(this Game game, IReadOnlyList<IReadOnlyList<int>> input)
+    {
+        var columns = new List<Row>();
+        for (var i = 0; i < 9; i++)
+        {
+            var column = new Row();
+            var fields = new List<Field>();
+            for (var j = 0; j < 9; j++)
+            {
+                var field = new Field()
+                {
+                    Value = input[j][i],
+                    isOrginalValue = input[j][i] != 0,
+                    FieldId = (((j + 1) * 9) + i) - 9
+                };
+                fields.Add(field);
+            }
+
+            column.Fields = fields;
+            columns.Add(column);
+        }
+
+        game.Columns = columns;
+    }
+
+    private static void ImportSquare(this Game game)
+    {
+        var squares = new List<List<Field>>();
+        var bigSquares = new List<List<Field>>();
+        var finalSquares = new List<List<Field>>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            bigSquares.Add(new List<Field>());
+        }
+
+        for (var i = 0; i < 9; i++)
+        {
+            for (var j = 0; j < 3; j++)
+            {
+                squares.Add(game.Columns.ToList()[i].Fields.Skip(j * 3).Take(3).ToList());
+            }
+        }
+
+        for (var i = 0; i < 27; i++)
+        {
+            bigSquares[i % 3].AddRange(squares[i]);
+        }
+
+        for (var i = 0; i < 3; i++)
+        {
+            for (var j = 0; j < 9; j++)
+            {
+                finalSquares.Add(bigSquares[i].Skip(j * 3).Take(3).ToList());
+            }
+        }
+
+        var result = new List<Row>();
+
+        for (var i = 0; i < 9; i++)
+        {
+            var fields = new List<Field>();
+            for (var j = 0; j < 3; j++)
+            {
+                fields.AddRange(finalSquares[i * 3 + j]);
+            }
+
+            result.Add(new Row() { Fields = fields });
+        }
+
+        game.Squares = result.Select(x => new Row() { Fields = x.Fields.OrderBy(c => c.FieldId).ToList() });
+
+    }
+
+    private static void ImportFields(this Game game, IReadOnlyList<IReadOnlyList<int>> input)
+    {
+        var fields = new List<Field>();
+        for (var i = 0; i < 9; i++)
+        {
+            for (var j = 0; j < 9; j++)
+            {
+                fields.Add(new Field()
+                {
+                    Value = input[j][i],
+                    isOrginalValue = input[j][i] != 0,
+                    FieldId = (((j + 1) * 9) + i) - 9
+                });
+            }
+        }
+
+        game.Fields = fields;
+    }
+}
